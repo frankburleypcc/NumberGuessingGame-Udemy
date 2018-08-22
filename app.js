2a97edb681e3cf46d0424ff6805fb1aff9d28b3e
frankburleypcc <frank.burley@pcc.edu>
2018-08-21 21:34:14 -0700

    Part 2 complete. JS with winning results and error for not entering within min and max

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-console.log('hi')
+// Game Function
+
+// Play must guess a number between min and max
+// Player get a certain amount of guesses
+// Notify player of guesses remaining
+// Notify of the player of the correct answer if they lose
+// Let the player choose to play again
+
+// Game Values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn= document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI Min and Max
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+
+guessBtn.addEventListener('click', function() {
+    let guess = parseInt(guessInput.value);
+
+    // Validate Input
+    if(isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+
+    }
+    // Check if won
+    if(guess === winningNum) {
+        // Disable Input
+        guessInput.disabled = true;
+        // Change border input to show winning answer
+        guessInput.style.borderColor = 'green';
+        // Let user know that they won
+        setMessage(`${winningNum} is correct. YOU WIN!!`, 'green');
+    } else {
+
+    }
+});
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}
+
+
