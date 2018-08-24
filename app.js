57fa3ec62db4d3d086d622c1eff1b82d3e4c58dc
frankburleypcc <frank.burley@pcc.edu>
2018-08-23 21:06:59 -0700

    Part 3 complete. Lose case and gane over. Set the input to clear, added gameOver function with ternary

--- app.js
+++ app.js
@@ -38,17 +38,52 @@ guessBtn.addEventListener('click', function() {
     }
     // Check if won
     if(guess === winningNum) {
-        // Disable Input
-        guessInput.disabled = true;
-        // Change border input to show winning answer
-        guessInput.style.borderColor = 'green';
-        // Let user know that they won
-        setMessage(`${winningNum} is correct. YOU WIN!!`, 'green');
-    } else {
+        // Game over - WON
 
+        gameOver(true, `${winningNum} is correct. YOU WIN!!`);
+    } else {
+        // Wrong number 
+        guessesLeft -= 1;
+
+        if (guessesLeft === 0) {
+            // Game over - LOST
+            
+            gameOver(false, `Game Over. You Lost. The correct number was ${winningNum}`);
+
+        } else {
+            // Game Continues - Answer Wrong
+
+            // Change border color
+            guessInput.style.borderColor = 'red';
+
+            // Clear the input
+            guessInput.value = '';
+
+
+            // Tell user it is the wrong number. 
+            setMessage(`${guess} is not correct. ${guessesLeft} guesses left.`);
+
+
+        }
     }
 });
 
+// Game over
+function gameOver(won, msg) {
+     let color;
+     won === true ? color = 'green' : 'red';
+
+     // Disable Input
+     guessInput.disabled = true;
+     // Change border input to show winning answer
+     guessInput.style.borderColor = color;
+     // Set text color
+     message.style.color = color;
+     // Let user know that they won
+     setMessage(msg);
+}
+
+// Set Message
 function setMessage(msg, color) {
     message.style.color = color;
     message.textContent = msg;
